a8ec1d66d2c44776361c0521ec654d42dbf60c0c
code-architect <codearchitectdeveloper@gmail.com>
2018-06-03 03:42:02 +0530

    Assigning students to courses and adding new course with teachers

--- app/Http/Controllers/CourseStudentController.php
+++ app/Http/Controllers/CourseStudentController.php
@@ -2,16 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Course;
+use App\Model\Student;
+
 class CourseStudentController extends Controller{
 
-    public function index()
+    public function index($id)
     {
-        return __METHOD__;
+        $course = Course::find($id);
+
+        if($course)
+        {
+            $students = $course->students;
+            return $this->createSuccessResponse($students, 200);
+        }
+        return $this->createErrorResponse("Does not exists a course with the given id", 404);
     }
 
-    public function store()
+    public function store($course_id, $student_id)
     {
-        return __METHOD__;
+        $course = Course::find($course_id);
+        if($course)
+        {
+            $student = Student::find($student_id);
+            if($student)
+            {
+                if($course->students()->find($student->id))
+                {
+                    return $this->createErrorResponse("The student with ID: {$student->id} already doing this course", 409);
+                }
+                $course->students()->attach($student->id);
+                return $this->createSuccessResponse("The student with ID: {$student->id} added to the course", 201);
+            }
+            return $this->createErrorResponse("Does not exists a student with the given id", 404);
+        }
+        return $this->createErrorResponse("Does not exists a course with the given id", 404);
     }
 
     public function destroy()
--- app/Http/Controllers/TeacherCourseController.php
+++ app/Http/Controllers/TeacherCourseController.php
@@ -2,16 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Course;
+use App\Model\Teacher;
+use Illuminate\Http\Request;
+
 class TeacherCourseController extends Controller{
 
-    public function index()
+    public function index($id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($id);
+
+        if($teacher)
+        {
+            $courses = $teacher->courses;
+            return $this->createSuccessResponse($courses, 200);
+        }
+        return $this->createErrorResponse("Does not exists a course with the given id", 404);
     }
 
-    public function store()
+    public function store(Request $request, $teacher_id)
     {
-        return __METHOD__;
+        $teacher = Teacher::find($teacher_id);
+        if($teacher)
+        {
+            $this->validateRequest($request);
+
+            $course = Course::create([
+                'title' =>  $request->get('title'),
+                'description' =>  $request->get('description'),
+                'value' =>  $request->get('value'),
+                'teacher_id' =>  $teacher_id,
+            ]);
+            return $this->createSuccessResponse("The course with id: {$course->id} has been created", 201);
+        }
+        return $this->createErrorResponse("Teacher does not exists with the given id", 404);
     }
 
 
@@ -24,6 +48,16 @@ class TeacherCourseController extends Controller{
     {
         return __METHOD__;
     }
+//---------------------------------------------------------------------------------------------------//
 
+    public function validateRequest($request)
+    {
+        $rules =[
+            'title' => 'required',
+            'description' => 'required',
+            'value' => 'required'
+        ];
+        $this->validate($request, $rules);
+    }
 
 }
